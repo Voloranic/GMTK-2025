4c2f4bd85d14de21641f609db0f27204f7f5ca34
amir-m-shulman <amir.shulman08@gmail.com>
2025-07-31 01:55:25 +0300

    loop gravityyyyy

---
+++ Game/GMTK Project 2025/Assets/Scripts/Volter/GroundDirection.cs
@@ -0,0 +1,93 @@
+using System.Runtime.InteropServices;
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+
+public class GroundDirection : MonoBehaviour
+{
+    [SerializeField] LayerMask GroundLayer;
+    RaycastHit2D[] search = new RaycastHit2D[4];
+    float[] rayDistance = { 0, 0, 0, 0 };
+    RaycastHit2D hit;
+    RaycastHit2D search1;
+    RaycastHit2D search2;
+    RaycastHit2D search3;
+    RaycastHit2D search4;
+
+    Vector2 gravityDirection;
+    Vector2 playerDown;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        search[0] = search1;
+        search[1] = search2;
+        search[2] = search3;
+        search[3] = search4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void FixedUpdate()
+    {
+        ApplyGravity();
+    }
+    private void ApplyGravity()
+    {
+
+        // oppesite of up
+        playerDown = -transform.parent.up;
+
+        //cast ray to the ground
+        hit = Physics2D.Raycast(transform.position, playerDown, 3, GroundLayer);
+        Debug.DrawRay(transform.position, playerDown * 3, Color.red);
+
+        if (hit.collider != null)
+        {
+          
+            // Set gravity to pull the player towards the hit normal.
+            gravityDirection = -hit.normal;
+
+            // rotate the player to match the ground normal.
+            transform.parent.up = hit.normal;
+        }
+        else
+        {
+            //if you fly and have no ground
+            //shoot 4 rays to find the closest ground and get the distance of each from the ground
+
+            search[0] = Physics2D.Raycast(transform.position, transform.up, 50, GroundLayer);
+            Debug.DrawRay(transform.position, transform.up * 50, Color.cyan);
+            rayDistance[0] = search[0].distance;
+            search[1] = Physics2D.Raycast(transform.position, -transform.up, 50, GroundLayer);
+            Debug.DrawRay(transform.position, -transform.up * 50, Color.cyan);
+            rayDistance[1] = search[1].distance;
+            search[2] = Physics2D.Raycast(transform.position, transform.right, 50, GroundLayer);
+            Debug.DrawRay(transform.position, transform.right * 50, Color.cyan);
+            rayDistance[2] = search[2].distance;
+            search[3] = Physics2D.Raycast(transform.position, -transform.right, 50, GroundLayer);
+            Debug.DrawRay(transform.position, -transform.right * 50, Color.cyan);
+            rayDistance[3] = search[3].distance;
+
+            //find the closest one and assign it as the new ground
+            int smallest = 0;
+            for(int i = 0; i < search.Length; i++)
+            {
+                if (rayDistance[smallest] > rayDistance[i])
+                {
+                    smallest = i;
+                }
+            }
+
+            //apply new gravity and rotation to gravityDirection
+            gravityDirection = -search[smallest].normal;
+            transform.parent.up = search[smallest].normal;
+           
+
+
+
+        }
+        // apply the final gravity direction.
+        Physics2D.gravity = gravityDirection * 9.8f;
+    }
+}
