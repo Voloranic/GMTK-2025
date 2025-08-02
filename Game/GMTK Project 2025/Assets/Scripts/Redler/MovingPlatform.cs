9605035adb1302f29bd8518d5bef0e9408e89024
Voloranic <voloranic@gmail.com>
2025-08-02 23:19:02 +0300

    movingplatform gogogogo

---
+++ Game/GMTK Project 2025/Assets/Scripts/Redler/MovingPlatform.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    [SerializeField] float moveSpeed = 5f;
+
+    int direction = 1;
+
+    Vector2 localVelocity;
+
+    public enum MoveAxis
+    {
+        X, Y
+    }
+    [SerializeField] MoveAxis moveAxis = MoveAxis.X;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        UpdateVelocity();
+    }
+
+    private void UpdateVelocity()
+    {
+        localVelocity = transform.InverseTransformDirection(rb.linearVelocity);
+
+        if (moveAxis == MoveAxis.X)
+        {
+            localVelocity.x = moveSpeed * direction;
+        }
+        else
+        {
+            localVelocity.y = moveSpeed * direction;
+        }
+
+        rb.linearVelocity = transform.TransformDirection(localVelocity);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("SwitchMovement"))
+        {
+            print("Gyattttttttt");
+            direction *= -1;
+            UpdateVelocity();
+        }
+    }
+}
