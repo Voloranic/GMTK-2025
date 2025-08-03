1d26e8dbde62f1b174da97696c49c255e969c615
Voloranic <voloranic@gmail.com>
2025-08-03 13:08:00 +0300

    בולבוליני בתחתיני

--- Game/GMTK Project 2025/Assets/Scripts/Redler/MovingPlatform.cs
+++ Game/GMTK Project 2025/Assets/Scripts/Redler/MovingPlatform.cs
@@ -51,4 +51,19 @@ public class MovingPlatform : MonoBehaviour
             UpdateVelocity();
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.parent = null;
+        }
+    }
 }
